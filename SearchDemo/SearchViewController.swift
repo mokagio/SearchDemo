185ad190e0e79a254a21e5580826a262d4fd31d5
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-05 20:02:43 +1100

    Manage container view size in regard to keyboard being displayed/hidden

--- SearchDemo/SearchViewController.swift
+++ SearchDemo/SearchViewController.swift
@@ -4,6 +4,7 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var scrollViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet var segementControl: UISegmentedControl!
 
     let leftTableView = UITableView()
@@ -16,6 +17,20 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
 
         scrollView.pagingEnabled = true
         scrollView.delegate = self
+
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "keyboardWillBeShown:",
+            name: UIKeyboardWillShowNotification,
+            object: .None
+        )
+
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "keyboardWillBeHide:",
+            name: UIKeyboardWillHideNotification,
+            object: .None
+        )
     }
 
     override func viewDidLayoutSubviews() {
@@ -57,5 +72,81 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
         }
 
         segementControl.selectedSegmentIndex = pageIndex
+    }
+
+    // MARK: Keyboard
+
+    func keyboardWillBeShown(notification: NSNotification) {
+        guard let (duration, keyboardHeight, curveOption) = keyboardAnimationInfo(fromNotification: notification) else {
+            return
+        }
+
+        UIView.animateWithDuration(
+            duration,
+            delay: 0,
+            options: [curveOption],
+            animations: { [weak self] in
+                self?.scrollViewBottomConstraint.constant = keyboardHeight
+                self?.view.layoutIfNeeded()
+            },
+            completion: .None
+        )
+    }
+
+    func keyboardWillBeHide(notification: NSNotification) {
+        guard let (duration, _, curveOption) = keyboardAnimationInfo(fromNotification: notification) else {
+            return
+        }
+
+        UIView.animateWithDuration(
+            duration,
+            delay: 0,
+            options: [curveOption],
+            animations: { [weak self] in
+                self?.scrollViewBottomConstraint.constant = 0
+                self?.view.layoutIfNeeded()
+            },
+            completion: .None
+        )
+    }
+
+    func keyboardAnimationInfo(fromNotification notification: NSNotification) -> (
+        NSTimeInterval, CGFloat, UIViewAnimationOptions
+        )? {
+            guard let infoDictionary = notification.userInfo else {
+                return .None
+            }
+
+            guard let duration = infoDictionary[UIKeyboardAnimationDurationUserInfoKey] as? NSTimeInterval else {
+                return .None
+            }
+
+            guard let keyboardFrame = (infoDictionary[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() else {
+                return .None
+            }
+
+            guard let rawValue = infoDictionary[UIKeyboardAnimationCurveUserInfoKey] as? Int else {
+                return .None
+            }
+            guard let animationCurve = UIViewAnimationCurve(rawValue: rawValue) else {
+                return .None
+            }
+
+            return (
+                duration,
+                keyboardFrame.size.height,
+                UIViewAnimationOptions.option(withCurve: animationCurve)
+            )
+    }
+}
+
+extension UIViewAnimationOptions {
+    static func option(withCurve curve: UIViewAnimationCurve) -> UIViewAnimationOptions {
+        switch curve {
+        case .EaseIn: return UIViewAnimationOptions.CurveEaseIn
+        case .EaseInOut: return UIViewAnimationOptions.CurveEaseInOut
+        case .EaseOut: return UIViewAnimationOptions.CurveEaseOut
+        case .Linear: return UIViewAnimationOptions.CurveLinear
+        }
     }
 }

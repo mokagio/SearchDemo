86ce8e8027d7919ed2567a23ce82b2ae8ffdbf1f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-05 21:02:09 +1100

    Prevent the scroll view from bouncing

--- SearchDemo/SearchViewController.swift
+++ SearchDemo/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
         segementControl.addTarget(self, action: "switchTableView", forControlEvents: .ValueChanged)
 
         scrollView.pagingEnabled = true
+        scrollView.bounces = false
         scrollView.delegate = self
         scrollView.showsHorizontalScrollIndicator = false
 
